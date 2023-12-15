cfa54a3f6a1a0d598e03d81cef413df02d7e4c01
Edilson Junior <edilsonrsjunior@gmail.com>
2023-12-15 09:38:44 -0400

    add some comments in importante parts of code

--- FASTAPI/api.py
+++ FASTAPI/api.py
@@ -25,6 +25,9 @@ load_dotenv()
 app = FastAPI()
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
+# ISSO PARA EVITAR O PROBLEMA DE CROSS 
+# ORIGIN QUANDO FEITO AS REQs HTTP PELO REACT
+
 from fastapi.middleware.cors import CORSMiddleware
 
 origins = [
@@ -50,10 +53,9 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# --------------------------------------------------------------------
 
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
+# CADASTRO DE NOVOS USUARIOS
 
 @app.post("/users")
 async def newUser(usuario: User):
@@ -62,12 +64,18 @@ async def newUser(usuario: User):
     try:
         cursor.execute(query,(usuario.nome, usuario.sobrenome,usuario.email, encryptPass(usuario.senha)))
         connection.commit()
+    # CASO NAO CONSIGA INSERIR POR MOTIVO DE O EMAIL QUE É A PK JA EXISTIR NO BD
+    # LANCAMOS UMA EXCECAO
     except:
         raise HTTPException(
             status_code= 401,
             detail="Email Already in Use"
         )
     
+# ------------------------------------------------------------------------
+
+# O LOGIN QUE GERA O TOKEN JWT, LEMBRANDO QUE O TOKEN DE VALIDADE DE 60 MINUTOS DEFINIDO PELA 
+# CONSTANTE <SESSION>
 
 @app.post("/login")
 async def loginUser(usuario: UserLogin):
@@ -75,6 +83,8 @@ async def loginUser(usuario: UserLogin):
 
     
     cursor.execute("SELECT COUNT(*) AS total FROM usuario WHERE email = %(usuario.email)s AND ativo = TRUE", {'usuario.email':usuario.email})
+    #TEMOS O FETCHONE PARA RETORNA A PRIMEIRA LINHA DA RESPOSTA DO BD 
+    #E O[0] para retornar apenas o primeiro valor ja que o padrão é retornar uma tupla ex.: (1,)
     result = cursor.fetchone()[0]
     if result == 0:
         raise HTTPException(
@@ -82,10 +92,11 @@ async def loginUser(usuario: UserLogin):
             detail="Usuário ou senha incorreto"
         )
     
+    # pega o id para associar ao token para ser utilizado em outras reqs
     cursor.execute("SELECT id,senha FROM usuario WHERE email = %s", (usuario.email,))
     result = cursor.fetchone()
     
-
+    # verifica se s a senha passada é igual a registrada no bd com o bcrypt
     if  not descriptPass(usuario.senha, result[1]):
         raise HTTPException(
             status_code= 401,
@@ -94,14 +105,16 @@ async def loginUser(usuario: UserLogin):
         
     # CREATE TOKEN
     # VAMOS CRIAR UM TOKEN TEMP
+    # o payload é a carga ou seja a informacao a ser salva no token
     payload = {
         "id" : result[0],
         "exp": datetime.datetime.now(tz=datetime.timezone.utc) + datetime.timedelta(minutes=SESSION_TIME)
          
     }
-    print(os.getenv("MY_JWT_KEY"))
     encodeToken = jwt.encode(payload,os.getenv("MY_JWT_KEY"),"HS256")
     return {"token" : encodeToken}
+
+#-----------------------------------------------------------------------------------------------------
 
 @app.get("/carros")
 async def listarCarros():
@@ -116,10 +129,16 @@ async def listarCarros():
             detail="Error to get data"
         )
 
+    # pega todas a linhas que a querry retorna
     result = cursor.fetchall()
 
+    # Cria um array para salvar tudo
     carros = []
     for carro in result:
+        # Aqui que que que temos uma detalhe legal, armazenamemos antes de salvar
+        # na lista a combinação dos valores de cursor.description que possui o nome e tipo dos
+        # campos do retorno da querry e após junta o primeiro valor de coluna ou seja o nome do campo
+        #com seu valor real
         carro_dict = dict(zip([coluna[0] for coluna in cursor.description], carro))
         carros.append(carro_dict)
 
@@ -127,6 +146,9 @@ async def listarCarros():
 
 
 @app.post("/carros")
+
+# Annotated[str,Form()] informa que o dado vira de um multipart 
+
 async def cadastrarCarro(nome: Annotated[str,Form()], marca: Annotated[str,Form()], modelo: Annotated[str,Form()], ano: Annotated[int, Form()], km: Annotated[float, Form()], valor: Annotated[float,Form()], desc: Annotated[str,Form()],image: Annotated[UploadFile, File()],token: Annotated[str, Depends(oauth2_scheme)]):
 
     try:
@@ -143,11 +165,6 @@ async def cadastrarCarro(nome: Annotated[str,Form()], marca: Annotated[str,Form(
                 status_code= 498,
                 detail="Expired Token"
             )
-    
-
-    # print("AUTENTICADO MEU CHEFE")
-
-    # car = Carro(nome=nome, marca=marca, modelo=modelo, valor=valor, desc=desc)
 
    
     #AGORA VAMOS SALVAr AS INFOs no BD e a URL DO CAR
@@ -157,8 +174,11 @@ async def cadastrarCarro(nome: Annotated[str,Form()], marca: Annotated[str,Form(
         cursor.execute(query,(nome, marca, modelo, ano, km,valor,desc,userId))
         connection.commit()
          #VAMOS SALVAr A IMG
+        # pega o ultimo id gerado pelo insert
         lastInsert = cursor.lastrowid
+        # espera o arquivo subir
         content = await image.read()
+        # espera a img subir para o Cloudnary
         urlImg = await uploadImg(content,userId)
         query = "UPDATE veiculosAnuncio SET photoUrl = %s WHERE id = %s"
         cursor.execute(query, (urlImg['url'],lastInsert))
@@ -249,8 +269,6 @@ async def updateCarro(nome: Annotated[str,Form()], marca: Annotated[str,Form()],
                     detail="Method Not Allowed to this Object"
                 )
 
-    ################################
-    # deleteImg(item_id)
     content = await image.read()
     urlImg = await uploadImg(content,userId)
     #AGORA VAMOS SALVAr AS INFOs no BD e a URL DO CAR
@@ -264,8 +282,4 @@ async def updateCarro(nome: Annotated[str,Form()], marca: Annotated[str,Form()],
                 status_code= 500,
                 detail="Internal Error"
             )
-    
 
-# @app.get("/items/{item_id}")
-# async def read_item(item_id: int, q: Union[str, None] = None):
-#     return {"item_id": item_id, "q": q}
