c89a5a3952bc9409ea248fa5ea52a5899c9d6c1f
Edilson Junior <edilsonrsjunior@gmail.com>
2023-12-16 16:09:30 -0400

    separate car private routes

--- FASTAPI/api.py
+++ FASTAPI/api.py
@@ -19,6 +19,8 @@ from pydantic import BaseModel
 from fastapi import FastAPI, Form
 from fastapi import FastAPI, File, UploadFile
 
+from carros import car
+
 SESSION_TIME = 60
 
 load_dotenv()
@@ -146,102 +148,4 @@ async def listarCarros():
     return json.dumps(carros)
 
 
-@app.post("/carros")
-
-# Annotated[str,Form()] informa que o dado vira de um multipart 
-
-async def cadastrarCarro(nome: Annotated[str,Form()], marca: Annotated[str,Form()], modelo: Annotated[str,Form()], ano: Annotated[int, Form()], km: Annotated[float, Form()], valor: Annotated[float,Form()], desc: Annotated[str,Form()],image: Annotated[UploadFile, File()],token: Annotated[str, Depends(oauth2_scheme)]):
-
-    userId = validateAccess(token)
-
-   
-    #AGORA VAMOS SALVAr AS INFOs no BD e a URL DO CAR
-    query = "INSERT INTO veiculosAnuncio (nome, marca, modelo, ano, km, valor, descricao,creator) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
-    cursor = connection.cursor()
-    try:
-        cursor.execute(query,(nome, marca, modelo, ano, km,valor,desc,userId))
-        connection.commit()
-         #VAMOS SALVAr A IMG
-        # pega o ultimo id gerado pelo insert
-        lastInsert = cursor.lastrowid
-        # espera o arquivo subir
-        content = await image.read()
-        # espera a img subir para o Cloudnary
-        urlImg = await uploadImg(content,userId)
-        query = "UPDATE veiculosAnuncio SET photoUrl = %s WHERE id = %s"
-        cursor.execute(query, (urlImg['url'],lastInsert))
-        connection.commit()
-    except:
-        raise HTTPException(
-                status_code= 500,
-                detail="Internal Error"
-            )   
-
-    return {"idPostCreated" : lastInsert}
-
-@app.delete("/carros/{item_id}")
-async def deleteCarro(item_id: int,token: Annotated[str, Depends(oauth2_scheme)]):
-    userId = validateAccess(token)
-    
-    #CHECK SE O POST É DA PESSOA 
-    query = "SELECT creator from veiculosAnuncio creator WHERE id = %s AND ativo = TRUE"
-    cursor = connection.cursor()
-    cursor.execute(query,(item_id,))
-    result = cursor.fetchone()
-
-    if not result:
-         raise HTTPException(
-                status_code= 404,
-                detail="Object Not Found"
-            )
-    
-    if not isAdminUser(userId):
-        if not (result[0] == userId):
-            raise HTTPException(
-                    status_code= 405,
-                    detail="Method Not Allowed to this Object"
-                )
-
-    query = "UPDATE veiculosAnuncio SET ativo = 0 WHERE id = %s"
-    cursor = connection.cursor()
-   
-    cursor.execute(query,(item_id,))
-    connection.commit()
-
-@app.put("/carros/{item_id}")
-async def updateCarro(nome: Annotated[str,Form()], marca: Annotated[str,Form()], modelo: Annotated[str,Form()], ano: Annotated[int, Form()], km: Annotated[float, Form()], valor: Annotated[float,Form()], desc: Annotated[str,Form()],image: Annotated[UploadFile, File()],item_id: int,token: Annotated[str, Depends(oauth2_scheme)]):   
-    userId = validateAccess(token)
-    
-    #CHECK SE O POST É DA PESSOA 
-    query = "SELECT creator from veiculosAnuncio creator WHERE id = %s AND ativo = TRUE"
-    cursor = connection.cursor()
-    cursor.execute(query,(item_id,))
-    result = cursor.fetchone()
-
-    if not result:
-         raise HTTPException(
-                status_code= 404,
-                detail="Object Not Found"
-            )
-    
-    if not isAdminUser(userId):
-        if not (result[0] == userId):
-            raise HTTPException(
-                    status_code= 405,
-                    detail="Method Not Allowed to this Object"
-                )
-
-    content = await image.read()
-    urlImg = await uploadImg(content,userId)
-    #AGORA VAMOS SALVAr AS INFOs no BD e a URL DO CAR
-    query = "UPDATE veiculosAnuncio SET nome = %s, marca = %s, modelo = %s, ano =%s, km = %s, valor = %s, descricao = %s, photoUrl = %s WHERE id = %s"
-    cursor = connection.cursor()
-    try:
-        cursor.execute(query,(nome, marca, modelo, ano, km, valor,desc,urlImg['url'],item_id))
-        connection.commit()
-    except:
-        raise HTTPException(
-                status_code= 500,
-                detail="Internal Error"
-            )
-
+app.mount("/carros/", car)
